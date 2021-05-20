33dd3c7d156002b80c5c912b6b4839928c033bf4
Stefano Della Chiesa <stefano.dellachiesa@gmail.com>
2021-05-20 15:41:29 +0200

    TEsting API KEY connection

--- scripts/useCase1.R
+++ scripts/useCase1.R
@@ -3,13 +3,23 @@
 # Author: Lukas Egli
 # Date: 19/05/2021
 
+#---- Load required packages
 if (!require("ckanr")) install.packages("ckanr");library ("ckanr")
 if (!require("raster")) install.packages("raster");library ("raster")
 if (!require("rgdal")) install.packages("rgdal");library ("rgdal")
 
+#---- Testing Packages for provenance tracking not working (problem with dependency package histry)
+#if (!require("trackr")) install.packages("trackr");library ("trackr")
+
+
+
 ########################## Data download
 ## Connect to GeoKur CKAN and retrieve Permanent crops for human consumption
-ckanr_setup("https://geokur-dmp.geo.tu-dresden.de/")
+
+# Stefano API KEY
+ckanr_setup("https://geokur-dmp.geo.tu-dresden.de/", key = "42c17265-5741-4a73-9668-fb9535b13d47")
+
+# 
 pollination <- raster(resource_show(id = "09be0d5d-e67a-43ce-b80b-7e27861bbd13")$url)
 yieldRapeseed <- raster(resource_show(id = "945acf8d-925f-44c5-8f45-4b6354f1734d")$url)
 
@@ -32,7 +42,12 @@ outputTableFinal <- outputTable[which(outputTable$yieldRapeseed>0&!is.na(outputT
 head(outputTableFinal) ## this would be the DATA OUTPUT!
 
 # store data -> not working yet!!!
-res <- package_create(name = "yields-and-pollination-europe", author="Lukas Egli")
+res <- package_create(name = "yields-and-pollination-europe",
+                      author="Lukas Egli",
+                      contact_name = "Lukas Egli",
+                      owner_org = "TUD",
+                      theme = "BioDiv",
+                      was_generated_by = "NA")
 # then create a resource
 file <- system.file("examples", outputTableFinal, package = "ckanr")
 (xx <- resource_create(package_id = res$id,
