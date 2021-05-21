fd7dac517c992bfa80367851c7a4c3686af7df3d
Stefano Della Chiesa <stefano.dellachiesa@gmail.com>
2021-05-21 14:41:25 +0200

    - Script formtatted - Fixed problem with Create Package Function - Package successfully pushed to CKAN - Resources (lm output and table output) added to the CKAN package

--- scripts/useCase1.R
+++ scripts/useCase1.R
@@ -8,28 +8,31 @@ if (!require("ckanr")) install.packages("ckanr");library ("ckanr")
 if (!require("raster")) install.packages("raster");library ("raster")
 if (!require("rgdal")) install.packages("rgdal");library ("rgdal")
 
-#---- Testing Packages for provenance tracking not working (problem with dependency package histry)
-#if (!require("trackr")) install.packages("trackr");library ("trackr")
+
+#---- 
+## PROVENANCE (in progress)
+#Testing Packages for provenance tracking --> TBD
 
 
-
-########################## Data download
-## Connect to GeoKur CKAN and retrieve Permanent crops for human consumption
-
-#  BEFORE making public the repository REMEMBER to remove from history the CAN API KEY 
+# ----
+## CONFIGURE CKAN CONNECITON
+#(BEFORE making public the repository REMEMBER to remove from history the CAN API KEY) 
 ckanr_setup("https://geokur-dmp.geo.tu-dresden.de/", key = "42c17265-5741-4a73-9668-fb9535b13d47")
 
-
-
-# 
+# ----
+## LOAD LOCALLY REMOTE RESOURCE FROM CKAN
+# Retrieve Permanent crops for human consumption and pollination
 pollination <- raster(resource_show(id = "09be0d5d-e67a-43ce-b80b-7e27861bbd13")$url)
 yieldRapeseed <- raster(resource_show(id = "945acf8d-925f-44c5-8f45-4b6354f1734d")$url)
 
 
+# ----
 
-########################## Data preparation
+## DATA PREPROCESSING
+
 # @prov [pollinationProj] = projectRaster(pollination) {basal change}
 pollinationProj <- projectRaster(pollination, crs="+proj=longlat +datum=WGS84 +no_defs ") # change crs
+
 # @prov [pollinationRes] = resample(pollination,yieldRapeseed) {value change}
 pollinationRes <- resample(pollinationProj,yieldRapeseed) # resample to 5 arcmin
 plot(pollinationRes,xlim=c(-20,50),ylim=c(20,70))
@@ -43,27 +46,47 @@ names(outputTable) <- c("yieldRapeseed","pollination")
 outputTableFinal <- outputTable[which(outputTable$yieldRapeseed>0&!is.na(outputTable$pollination)),] # remove 0 yields and NAs
 head(outputTableFinal) ## this would be the DATA OUTPUT!
 
-# store data -> Got the following error so far: Access denied: User "xyz" not authorized to add dataset to this organization
-res <- package_create(name = "yields-and-pollination-europe",
-                      owner_org = "tud",
-                      theme = "https://inspire.ec.europa.eu/theme/au",
-                      contact_name = "lukas egli",
-                      was_generated_by = "NA")
-# then create a resource
-file <- system.file("examples", outputTableFinal, package = "ckanr")
-(xx <- resource_create(package_id = res$id,
-                       description = "my resource",
-                       name = "bears",
-                       upload = file,
-                       rcurl = "https://geokur-dmp.geo.tu-dresden.de/dataset"
-))
-ds_create(resource_id = xx$id, records = iris, force = TRUE)
-resource_show(xx$id)
+write.csv(outputTableFinal,"myOutputTable.csv")
+
 
 ########################## Data analysis
 # @prov [modelRapeseed] = lm(yieldRapeseed,pollinationRes) {core concept}
 # using original inputs instead of the output table is maybe more informative regarding provenance?
 modelRapeseed <- lm(yieldRapeseed~pollination,data=outputTableFinal) ## this would be the MODEL OUTPUT!
-summary(modelRapeseed)
 
-rm(list=ls())
+
+sink("LinearModelOutput.txt")
+print(summary(modelRapeseed))
+sink()  # returns output to the console
+
+
+## CREATE PACKAGE & PUSH IT TO CKAN
+res <- package_create(name = "test-file-yields-and-pollination-europe",
+                      owner_org = "tud",
+                      extras = c(was_generated_by = "test", contact_name = "arne", theme = "test"))
+
+
+#----
+## CREATE RESOURCE (outputTableFinal) & PUSH IT TO CKAN --> UPDATE PACKAGE
+#file <- system.file("examples", outputTableFinal, package = "ckanr")
+(xx <- resource_create(package_id = res$id,
+                       description = "mymyOutputTableFinal  resource",
+                       name = "myOutputTableFinal",
+                       upload = "./myOutputTable.csv",
+                       rcurl = "https://geokur-dmp.geo.tu-dresden.de/dataset"
+))
+
+## CREATE RESOURCE (LinearModelOutput) & PUSH IT TO CKAN --> UPDATE PACKAGE
+(xx <- resource_create(package_id = res$id,
+                       description = "my LinearModelOutput resource",
+                       name = "LinearModelOutput",
+                       upload = "./LinearModelOutput.txt",
+                       rcurl = "https://geokur-dmp.geo.tu-dresden.de/dataset"
+))
+
+
+#ds_create(resource_id = xx$id, records = iris, force = TRUE)
+resource_show(xx$id)
+
+
+#rm(list=ls())
