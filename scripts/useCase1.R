9c149b50855c3a71a614c4fc677a942a4c2a66da
legli <lukas.egli@ufz.de>
2021-07-07 12:06:55 +0200

    updated provenance

--- scripts/useCase1.R
+++ scripts/useCase1.R
@@ -18,64 +18,59 @@ if (!require("rgdal")) install.packages("rgdal");library ("rgdal")
 ## CONFIGURE CKAN CONNECITON
 #(BEFORE making public the repository REMEMBER to remove from history the CAN API KEY) 
 ckanr_setup("https://geokur-dmp.geo.tu-dresden.de/", key = "42bec965-954b-4898-bc76-a18274bbc982")
+ckanr_setup("https://geokur-dmp.geo.tu-dresden.de/", key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MjM0MTk5MTAsImp0aSI6Imprc0FrVnlRTHMzT1hjQS1PZXZjNGRVck1sc3g5SmptaGNKUmEyMjNNWWRhVkc0WHBKX3BuU3daSEZtS2tqVm03a2JUb0JEd19zT1VIN2Y4In0.HvWSVGr6RYmKXkyLCNQtMVmM_GB-dGLsJ9xtzLiQwW4")
+
+
+# browse ckan available datasets, package list gives the human-readable identifieres of every public dataset. The API refers to those human-readable identifieres as
+# "name". In the CKAN Webpage we call them "Identifier".
+ckan_available_datasets <- package_list()
+
+# access the metadata of a given dataset by its "name"
+# package_show returns an R List object
+pollination_metadata <- package_show("demand-and-supply-of-pollination-in-the-european-union-test") 
+yieldRapeseed_metadata <- package_show("rapeseed-yield")
 
 # ----
 ## LOAD LOCALLY REMOTE RESOURCE FROM CKAN
 # Retrieve Permanent crops for human consumption and pollination
-pollination <- raster(resource_show(id = "09be0d5d-e67a-43ce-b80b-7e27861bbd13")$url)
-yieldRapeseed <- raster(resource_show(id = "945acf8d-925f-44c5-8f45-4b6354f1734d")$url)
-
-
-# ---- CREATE INPUT 1 ----
-input_dataset_pollination <- package_create(
-  extras = c(
-    name = "input-input_dataset_pollination",
-    owner_org = "ufz",
-    contact_name = "lukas",
-    theme = "https://inspire.ec.europa.eu/theme/au"
-  )
-)
-
-# ---- CREATE INPUT 2 ----
-input_dataset_yieldRapeseed <- package_create(
-  extras = c(
-    name = "input_dataset_yieldRapeseed",
-    owner_org = "ufz",
-    contact_name = "lukas",
-    theme = "https://inspire.ec.europa.eu/theme/au"
-  )
-)
+# get resource download url (target resource is nb. one in resource list)
+target_index <- 1
+download_url_pollination <- pollination_metadata$resource[[target_index]]$url
+download_url_yieldRapessed <- yieldRapeseed_metadata$resource[[target_index]]$url
+pollination <- raster(download_url_pollination)
+yieldRapeseed <- raster(download_url_yieldRapessed)
 
 
 ########################## DATA PREPROCESSING
-
-# project raster
+# # project raster
 pollinationProj <- projectRaster(pollination, crs="+proj=longlat +datum=WGS84 +no_defs ") # change crs
 # ---- CREATE INTERMEDIATE DATASET 1 ----
-intermediate_dataset_pollinationProj <- package_create(
+intermediate_dataset_pollination_proj <- package_create(
   extras = c(
-    name = "intermediate_dataset_pollinationProj",
+    name = "intermediate_dataset_pollination_proj",
+    conforms_to = "http://www.opengis.net/def/crs/OGC/1.3/CRS84",
     owner_org = "ufz",
     contact_name = "lukas",
     theme = "https://inspire.ec.europa.eu/theme/au",
-    was_derived_from = input_dataset_pollination$name,
-    was_generated_by = '{"uri": "XYZ", "label": "Project"}'
+    was_derived_from = pollination_metadata$name
+  )
+)
+# at this point we could also upload the reprojected resource
+
+# # resample to 5 arcmin
+pollinationRes <- resample(pollinationProj,yieldRapeseed) 
+# ---- CREATE INTERMEDIATE DATASET 2 ----
+intermediate_dataset_pollination_res <- package_create(
+  extras = c(
+    name = "intermediate_dataset_pollination_res",
+    conforms_to = "http://www.opengis.net/def/crs/OGC/1.3/CRS84",
+    owner_org = "ufz",
+    contact_name = "lukas",
+    theme = "https://inspire.ec.europa.eu/theme/au",
+    was_derived_from = intermediate_dataset_pollination_proj$name
   )
 )
 
-# resample to 5 arcmin
-pollinationRes <- resample(pollinationProj,yieldRapeseed) 
-# ---- CREATE INTERMEDIATE DATASET 2 ----
-intermediate_dataset_pollinationRes <- package_create(
-  extras = c(
-    name = "intermediate_dataset_pollinationRes",
-    owner_org = "ufz",
-    contact_name = "lukas",
-    theme = "https://inspire.ec.europa.eu/theme/au",
-    was_derived_from = intermediate_dataset_pollinationProj$name,
-    was_generated_by = '{"uri": "XYZ", "label": "Resample"}'
-  )
-)
 plot(pollinationRes,xlim=c(-20,50),ylim=c(20,70))
 plot(yieldRapeseed,xlim=c(-20,50),ylim=c(20,70))
 
@@ -93,65 +88,86 @@ write.csv(outputTableFinal,"myOutputTable.csv")
 # ---- CREATE OUTPUT DATASET ----
 output_dataset <- package_create(
   extras = c(
-    name = "output-dataset",
+    name = "output_dataset",
+    conforms_to = "http://www.opengis.net/def/crs/OGC/1.3/CRS84",
     owner_org = "ufz",
     contact_name = "lukas",
     theme = "https://inspire.ec.europa.eu/theme/au",
-    was_derived_from = paste(input_dataset_yieldRapeseed$name, intermediate_dataset_pollinationRes$name, sep=","), 
-    was_generated_by = '{"uri": "XYZ", "label": "Cbind"}'
+    was_derived_from = paste(yieldRapeseed_metadata$name, intermediate_dataset_pollination_res$name, sep=",") 
   )
 )
 
 
-
-package_delete(id = input_dataset_pollination$name)
-package_delete(id = input_dataset_yieldRapeseed$name)
-package_delete(id = intermediate_dataset_pollinationProj$name)
-package_delete(id = intermediate_dataset_pollinationRes$name)
-package_delete(id = output_dataset$name)
-
-
-
-
-
+# upload dataset
+resource_create(package_id = output_dataset$id,
+                      description = "myOutputTableFinal",
+                      name = "myOutputTableFinal",
+                      upload = "./myOutputTable.csv",
+                      rcurl = "https://geokur-dmp.geo.tu-dresden.de/dataset"
+)
+                      
 ########################## Data analysis
-# @prov [modelRapeseed] = lm(yieldRapeseed,pollinationRes) {core concept}
-# using original inputs instead of the output table is maybe more informative regarding provenance?
 modelRapeseed <- lm(yieldRapeseed~pollination,data=outputTableFinal) ## this would be the MODEL OUTPUT!
+save(modelRapeseed,file="modelRapeseed.RData")
+
+# ---- CREATE OUTPUT DATASET ----
+analysis_dataset <- package_create(
+  extras = c(
+    name = "model_rapeseed",
+    conforms_to = "http://www.opengis.net/def/crs/OGC/1.3/CRS84",
+    owner_org = "ufz",
+    contact_name = "lukas",
+    theme = "https://inspire.ec.europa.eu/theme/au",
+    was_derived_from = output_dataset$name
+  )
+)
+# upload dataset
+resource_create(package_id = analysis_dataset$id,
+                description = "myOutputTableFinal",
+                name = "myOutputTableFinal",
+                upload = "./modelRapeseed.Rdata",
+                rcurl = "https://geokur-dmp.geo.tu-dresden.de/dataset"
+)
+
+# package_delete(id = intermediate_dataset_pollination_proj$name)
+# package_delete(id = intermediate_dataset_pollination_res$name)
+# package_delete(id = output_dataset$name)
+# package_delete(id = analysis_dataset$name)
 
 
-sink("LinearModelOutput.txt")
-print(summary(modelRapeseed))
-sink()  # returns output to the console
-
-
-## CREATE PACKAGE & PUSH IT TO CKAN
-res <- package_create(name = "test-file-yields-and-pollination-europe",
-                      owner_org = "tud",
-                      extras = c(was_generated_by = "test", contact_name = "arne", theme = "test"))
-
-
-#----
-## CREATE RESOURCE (outputTableFinal) & PUSH IT TO CKAN --> UPDATE PACKAGE
-#file <- system.file("examples", outputTableFinal, package = "ckanr")
-(xx <- resource_create(package_id = res$id,
-                       description = "mymyOutputTableFinal  resource",
-                       name = "myOutputTableFinal",
-                       upload = "./myOutputTable.csv",
-                       rcurl = "https://geokur-dmp.geo.tu-dresden.de/dataset"
-))
-
-## CREATE RESOURCE (LinearModelOutput) & PUSH IT TO CKAN --> UPDATE PACKAGE
-(xx <- resource_create(package_id = res$id,
-                       description = "my LinearModelOutput resource",
-                       name = "LinearModelOutput",
-                       upload = "./LinearModelOutput.txt",
-                       rcurl = "https://geokur-dmp.geo.tu-dresden.de/dataset"
-))
-
-
-#ds_create(resource_id = xx$id, records = iris, force = TRUE)
-resource_show(xx$id)
+# 
+# sink("LinearModelOutput.txt")
+# print(summary(modelRapeseed))
+# sink()  # returns output to the console
+# 
+# 
+# ## CREATE PACKAGE & PUSH IT TO CKAN
+# res <- package_create(name = "test-file-yields-and-pollination-europe",
+#                       owner_org = "tud",
+#                       extras = c(was_generated_by = "test", contact_name = "arne", theme = "test"))
+# 
+# 
+# #----
+# ## CREATE RESOURCE (outputTableFinal) & PUSH IT TO CKAN --> UPDATE PACKAGE
+# #file <- system.file("examples", outputTableFinal, package = "ckanr")
+# (xx <- resource_create(package_id = res$id,
+#                        description = "myOutputTableFinal  resource",
+#                        name = "myOutputTableFinal",
+#                        upload = "./myOutputTable.csv",
+#                        rcurl = "https://geokur-dmp.geo.tu-dresden.de/dataset"
+# ))
+# 
+# ## CREATE RESOURCE (LinearModelOutput) & PUSH IT TO CKAN --> UPDATE PACKAGE
+# (xx <- resource_create(package_id = res$id,
+#                        description = "my LinearModelOutput resource",
+#                        name = "LinearModelOutput",
+#                        upload = "./LinearModelOutput.txt",
+#                        rcurl = "https://geokur-dmp.geo.tu-dresden.de/dataset"
+# ))
+# 
+# 
+# #ds_create(resource_id = xx$id, records = iris, force = TRUE)
+# resource_show(xx$id)
 
 
 #rm(list=ls())
